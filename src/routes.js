4b0b35733a89cb4c368aacea391ad1084b369ea0
Parisa Salimi <parisasalimi159@gmail.com>
2025-02-12 16:49:35 +0100

    Add new path to router file

--- src/routes.js
+++ src/routes.js
@@ -3,18 +3,22 @@ import MovieListPage from "@/pages/MovieListPage.vue";
 import MovieDetailPage from "@/pages/MovieDetailPage.vue";
 
 const routes = [
-    {
-      path: '/' ,
-      component: SearchPage ,
-    }, 
-    {
-      path: '/movies' ,
-      component: MovieListPage ,
-    },
-    {
-      path: '/movie/:id' ,
-      component : MovieDetailPage ,
-    }
+  {
+    path: "/",
+    component: SearchPage,
+  },
+  {
+    path: "/movies",
+    component: MovieListPage,
+  },
+  {
+    path: "/movie/:id",
+    component: MovieDetailPage,
+  },
+  {
+    path: "/test/:id",
+    component: MovieDetailPage,
+  },
 ];
 
 export default routes;
