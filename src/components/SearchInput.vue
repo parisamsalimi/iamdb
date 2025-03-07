10213a5b8f3f3db0c3801e3ed444b4114e96c456
Parisa Salimi <parisasalimi159@gmail.com>
2025-03-07 22:12:01 +0100

    active search by voice

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useRouter } from "vue-router";
+import { useSpeechRecognition } from "@vueuse/core";
 
 import searchIcon from "@/assets/searchIcon.svg";
 import mic from "@/assets/mic.svg";
@@ -8,8 +9,19 @@ import mic from "@/assets/mic.svg";
 const searchQuery = ref("");
 const router = useRouter();
 
+
+const { isListening, result, start, stop } = useSpeechRecognition();
+
+
+watch(result, (newResult) => {
+  if (newResult) {
+    searchQuery.value = newResult; 
+    searchMovies(); 
+  }
+});
+
 const searchMovies = () => {
-  if (!searchQuery.value) return;
+  if (!searchQuery.value.trim()) return;
 
   router.push({
     name: "SearchPage",
@@ -17,23 +29,34 @@ const searchMovies = () => {
     query: { q: searchQuery.value },
   });
 };
+
+
+const toggleListening = () => {
+  if (isListening.value) {
+    stop();
+  } else {
+    start();
+  }
+};
 </script>
+
 <template>
   <div class="search">
     <button class="input_icon" @click="searchMovies">
-      <img :src="searchIcon" alt="" />
+      <img :src="searchIcon" alt="Search Icon" />
     </button>
     <input
       class="search_input"
       v-model="searchQuery"
       @keyup.enter="searchMovies"
-      placeholder=""
+      placeholder="Search for movies..."
     />
-    <div id="microphone" class="input_icon">
-      <img :src="mic" alt="" />
-    </div>
+    <button id="microphone" class="input_icon" @click="toggleListening">
+      <img :src="mic" alt="Mic Icon" />
+    </button>
   </div>
 </template>
+
 
 <style>
 .search {
