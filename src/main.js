bfcdf8c473eed0d06a202fbc7b53c0b998a31c9a
Parisa Salimi <parisasalimi159@gmail.com>
2025-03-09 17:15:05 +0100

    update main.js

--- src/main.js
+++ src/main.js
@@ -4,13 +4,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import routes from "./routes.js";
 import { createPinia } from "pinia";
-
-import { createRouter, createWebHistory } 
-from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
- const store = createPinia();
-const app = createApp(Ap
+
+const store = createPinia();
+const app = createApp(App)
